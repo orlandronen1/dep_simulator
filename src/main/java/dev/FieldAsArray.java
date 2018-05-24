bc69d879d557924f81733ba40865e8047f9197b7
Unknown <roo18@pitt.edu>
2018-05-24 13:41:07 -0400

    More movement cases
    Current cases in the class: all vectors pointing in one direction, the field divided into opposing halves, a square loop around the border, and all vectors pointing to the center.

--- src/main/java/dev/FieldAsArray.java
+++ src/main/java/dev/FieldAsArray.java
@@ -14,23 +14,70 @@ public class FieldAsArray
 
     public static void main(String[] args) throws InterruptedException
     {
-        int fieldSize = 10;
-        double timeStep = 0.05;
-        Vector[][][] field = new Vector[fieldSize][fieldSize][fieldSize];
-        Particle particle = new TestParticle(0,0,0);
-        Vector position;
-        int x, y, z;
+        int fieldSize = 10;         // Length of a side of the field
+        double timeStep = 0.10;     // Increment of time to advance by per tick
+        Vector[][][] field = new Vector[fieldSize][fieldSize][fieldSize];   // Field is a 3D array, as a cube
+        Particle particle = new TestParticle(0,0,0);    // Particle to move
+        Vector position;            // Position of the particle
+        int x, y, z;                // Coordinates of the particle
         
+        // Field with all vectors pointing one way
         for (int i = 0; i < fieldSize; i++)
         {
             for (int j = 0; j < fieldSize; j++)
             {
                 for (int k = 0; k < fieldSize; k++)
-                {
-                    field[i][j][k] = new Vector(fieldSize - i,0,0);
-                }
+                    field[i][j][k] = new Vector(1,0,0);
             }
         }
+        
+        // Field where the vectors form a square loop, so the particle will never stop
+        // Note: particle must start on the origin/on an edge
+//        for (int i = 0; i < fieldSize - 1; i++)
+//            field[i][0][0] = new Vector(1,0,0);
+//        
+//        for (int i = 0; i < fieldSize - 1; i++)
+//            field[fieldSize - 1][i][0] = new Vector(0,1,0);
+//        
+//        for (int i = 0; i < fieldSize - 1; i++)
+//            field[fieldSize - i - 1][fieldSize - 1][0] = new Vector(-1,0,0);
+//        
+//        for (int i = 0; i < fieldSize - 1; i++)
+//            field[0][fieldSize - 1 - i][0] = new Vector(0,-1,0);
+        
+        // Field with opposing halves
+//        for (int i = 0; i < fieldSize; i++)
+//        {
+//            if (i < fieldSize/2)    x = 1;
+//            else                    x = -1;
+//            
+//            for (int j = 0; j < fieldSize; j++)
+//            {
+//                for (int k = 0; k < fieldSize; k++)
+//                    field[i][j][k] = new Vector(x,0,0);
+//            }
+//        }
+        
+        // Field where all vectors point to the center
+//        for (int i = 0; i < fieldSize; i++)
+//        {
+//            if (i < fieldSize/2)        x = 1;
+//            else                        x = -1;
+//            
+//            for (int j = 0; j < fieldSize; j++)
+//            {
+//                if (j < fieldSize/2)    y = 1;
+//                else                    y = -1;
+//                
+//                for (int k = 0; k < fieldSize; k++)
+//                {
+//                    if (k < fieldSize/2)    z = 1;
+//                    else                    z = -1;
+//                    
+//                    field[i][j][k] = new Vector(x,y,z);
+//                }
+//            }
+//        }
         
         while(true)
         {
